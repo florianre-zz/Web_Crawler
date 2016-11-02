470881dc2a502652f0bf9d4727a8ccbaab583756
fre14 <femile@blueyonder.co.uk>
2016-11-02 21:16:41 +0000

    Attempt depth first search of just links

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ def main():
     web_spider = WebSpider()
     links = web_spider.crawl('https://gocardless.com/')
 
-    root = etree.Element('map')
-    for link in links:
-        child = etree.SubElement(root, 'url')
-        child.text = link
-
+    tree = build_tree(links)
     doc = etree.ElementTree(root)
     outFile = open('output.xml', 'wb')
     doc.write(outFile, xml_declaration=False, pretty_print=True)
+
+def build_tree((url, links_accessible_via_url)):
+    root = etree.Element('url')
+    for elem in links_accessible_via_url:
+        if isinstance(elem, str)
+            child = etree.SubElement(root, 'url')
+            child.text = elem
+        else
+            root.append(build_tree(elem))
+
+    return root
 
 main()
